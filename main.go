f7ee7434ff4ef11a29b65d1d01eee020de17f236
선우 <ksw-12345@naver.com>
2023-06-15 18:45:03 +0900

    go-:=,var

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	const name string = "nico"
+	name := "nico"	//var name string = "nico"와 같다 function안에서 속성을 알아서 정의해줌
+	name = "lynn"
 	fmt.Println(name)
 }
